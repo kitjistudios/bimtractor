aaba4fd962bf5104365f85da36b952fa6ab82d8e
Shayne Marshall <smarshall@kitjistudios.com>
2018-04-20 12:09:05 -0400

    Add Media model

---
+++ app/Media.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Media extends Model {
+
+    //
+    public function CreateMedia($filetype, $filename) {
+       
+        return 1;
+    }
+
+}
