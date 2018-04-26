2787284f62f12ed8fa2e7290cd75f33671804b98
Shayne Marshall <smarshall@kitjistudios.com>
2018-04-26 14:50:18 -0400

    Add javascript code

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title></title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -66,23 +66,17 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
+
             <div class="top-right links">
-                @auth
-                <a href="{{ url('/home') }}">Home</a>
-                @else
-                <a href="{{ route('login') }}">Login</a>
-                <a href="{{ route('register') }}">Register</a>
-                @endauth
+                <a href="{{ __('home') }}">Home</a>
+
             </div>
-            @endif
+
 
             <div class="content">
                 <div class="title m-b-md">
-                    <img height="90px"  src="{{ asset('/bimtractor_logo.jpg') }}">   Bimtractor
-                 
-                </div>
-              
+                    <img height="90px"  src="{{ asset('/bimtractor_logo.jpg') }}">   Bimtractor                
+                </div>              
             </div>
         </div>
     </body>
