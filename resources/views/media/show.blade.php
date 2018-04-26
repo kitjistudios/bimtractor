aae8acc852352eeb32ec14f410fa54d24e23f7d3
Shayne Marshall <smarshall@kitjistudios.com>
2018-04-26 14:51:08 -0400

    Add Javascript code to capture store and display images

--- resources/views/media/show.blade.php
+++ resources/views/media/show.blade.php
@@ -1,2 +1,17 @@
 @extends('layouts.app')
 
+@section('content')
+<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+<script type="text/javascript">
+    $(document).ready(function () {
+
+
+       /* var dataImage = localStorage.getItem('imgData');
+        bannerImg = document.getElementById('tableBanner');
+        bannerImg.src = "data:image/png;base64," + dataImage;*/
+    });
+</script>
+<h1>show</h1>
+{{$media}} 
+<img src="" id="tableBanner" name="tableBanner" />
+@endsection
