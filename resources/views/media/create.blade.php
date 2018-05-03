29b1fcf1ff48e172202d6003f68143246996494e
Shayne Marshall <smarshall@kitjistudios.com>
2018-05-03 16:06:36 -0400

    Add styling to create image page

--- resources/views/media/create.blade.php
+++ resources/views/media/create.blade.php
@@ -1,18 +1,27 @@
-@extends('layouts.app')
-@section('content')
-<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+
+<script src="https://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
 <script type="text/javascript">
 
     $(document).ready(function () {
+        $("#MyImage").hide();
+        $("#MyPhoto").hide();
+        $("#SubmitMyPhoto").hide();
+        $("#CancelMyPhoto").hide();
+        $("#Description").hide();
 
         $("#MyPhoto").change(function () {
             var input = document.querySelector('input[type=file]'); // see Example 4
-            var file = input.files[0];   
+            var file = input.files[0];
             //upload(file);
-            drawOnCanvas(file);   // see Example 6
+            //drawOnCanvas(file);   // see Example 6
             displayAsImage(file); // see Example 7
 
         });
+        $("#CamIcon").click(function () {
+            //alert('hello');
+            $("#MyPhoto").trigger('click');
+        });
+
         function upload(file) {
             var form = new FormData(),
                     xhr = new XMLHttpRequest();
@@ -30,7 +39,6 @@
                         c = document.querySelector('canvas'), // see Example 4
                         ctx = c.getContext('2d'),
                         img = new Image();
-               
 
                 img.onload = function () {
                     c.width = img.width;
@@ -42,18 +50,26 @@
             };
 
             reader.readAsDataURL(file);
+            $("#PicFrame").append(img);
         }
 
         function displayAsImage(file) {
             var imgURL = URL.createObjectURL(file),
-                    img = document.createElement('img');
-
+                    //img = document.createElement('img');
+                    img = document.getElementById("MyImage");
             img.onload = function () {
                 URL.revokeObjectURL(imgURL);
             };
 
             img.src = imgURL;
-            document.body.appendChild(img);
+            img.height = 200;
+            img.width = 200;
+            $("#PicFrame").append(img);
+            $("#MyImage").show();
+            $("#Description").show();
+            $("#SubmitMyPhoto").show();
+            $("#CancelMyPhoto").show();
+            $("#Logo").hide();
         }
     });
 
@@ -82,39 +98,72 @@
      }*/
 
 </script>
-<div class="flex-center position-ref full-height">     
+@extends('layouts.app')
+@section('content')    
+<div class="container-fluid">
     <div class="content">
-        <div class="title m-b-md center-block">
-            <img height="90px" src="{{ asset('/bimtractor_logo.jpg') }}" id="MyLogo">   Bimtractor
-            <canvas id="mycanvas" name="mycanvas"></canvas>
-        </div>
+        <div class="row">
+            <div class="col-sm-3">
 
-        <div >
-            <form action="../media/store" method="POST" id="PhotoForm" >
-                @csrf 
-                <div class="form-group-sm">
-                    <div class="form-row ">
-                        <label class="text-center">Tell us about your snapshot</label>
-                        <input class="form-control" type="text" id="text1" name="text1" >                        
-                    </div> 
-                    <div class="form-inline">
-                        <label class="text-center"> <i class="fa fa-camera"></i>   Take a snapshot</label>
-                        <input class=" form-control-file" type="file"  accept="image/*" capture="user" id="MyPhoto" name="MyPhoto">
+            </div>
+            <div class="col-sm-6">
+                <div class="form-control" >
+
+                    <div class="form-row">
+                        <div class="col-sm-3">
+
+                        </div>
+                        <div class="col-sm-6">
+                            <form action="../media/store" method="POST" id="PhotoForm" >
+                                @csrf 
+                                <div class="row">
+                                    <div class="col-sm-2"></div>
+                                    <div class="col-sm-8">
+                                        <div class="row center" id="PicFrame">
+                                            <img class="center" alt="MyImage" id="MyImage">
+                                        </div>
+                                        <img src="{{asset('bimtractor_logo.jpg')}}" alt="{{ config('app.name') }}" height="150" id="Logo" name="Logo" > 
+                                    </div>
+                                    <div class="col-sm-2"></div>
+                                </div>    
+                                <div class="row mt-3">
+                                    <div class="col-sm-5"><button type="submit" id="SubmitMyPhoto" class="btn btn-light center text-uppercase" style="font-size: large;"><i class="fa fa-thumbs-up"></i>Keep it</button> </div>
+                                    <div class="col-sm-2"></div>
+                                    <div class="col-sm-5"><button type="submit" id="CancelMyPhoto" class="btn btn-light center text-uppercase" style="font-size: large;"><i class="fa fa-thumbs-down"></i>Toss it</button> </div>
+                                </div>                           
+                        </div>
+                        <div class="col-sm-3">
+
+                        </div>
                     </div>
-                    <caption> 
-
-
-                        <button type="submit" id="SubmitMyPhoto" class="btn btn-link">Store Snapshot</button>                      
                 </div>
-            </form>
-            <a href="../media/show">Show my pic</a>
+            </div>
+            <div class="col-sm-3">
+
+            </div>
+        </div> 
+
+        <div class="row mt-2">
+            <div class="col-sm-4">
+
+            </div>
+            <div class="col-sm-4">
+
+                <div class="form-control">
+                    <button class="btn btn-light center text-uppercase center"><i class="fa fa-camera" style="color: #000000; font-size: 30px" id="CamIcon"></i></button>
+                    <input class=" form-control-file" type="file"  accept="audio/*,video/*,image/*" capture="user" id="MyPhoto" name="MyPhoto"> 
+                    <div class="form-row details">
+                        <input class="form-control" type="text" id="Description" name="Description" autocomplete="off" placeholder="Tell us about your pic">                        
+                    </div> 
+                   </form>
+                </div>
+
+
+            </div>
+            <div class="col-sm-4">
+
+            </div>
         </div>
-        <footer class="flex-center">
-            <p>Powered by<a  href="">Kitji Studios</a></p>
-        </footer>
     </div>
-
-</div>   
-
-
+</div> 
 @endsection
