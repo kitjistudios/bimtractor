84c8f7a607f80021cb65c93fcb0fa82272e6add5
Shayne Marshall <smarshall@kitjistudios.com>
2018-05-03 16:17:26 -0400

    Recommit em all

--- routes/web.php
+++ routes/web.php
@@ -11,14 +11,14 @@
   |
  */
 
-Route::get('', function () {
+Route::get('/', function () {
     return view('welcome');
 });
 
 Auth::routes();
 
 //Route::get('home', 'HomeController@index')->name('home');
-Route::get('home', 'HomeController@index');
+Route::get('/home', 'HomeController@index');
 Route::get('media', 'MediaController@index');
 
 //Route::get('login', 'LoginController@index');
